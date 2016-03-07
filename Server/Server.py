0595045b72ed09d6cbb3ff4fbc6758bf3ae684c2
Finn Julius Stephansen-Smith <finnjs@stud.ntnu.no>
2016-03-07 15:06:43 +0100

    Did work on the server

--- Server/Server.py
+++ Server/Server.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
+import datetime
+import json
 import socketserver
 
 """
 Variables and functions that must be used by all the ClientHandler objects
 must be written here (e.g. a dictionary for connected clients)
 """
+
+# Dictionary
+clients = {}
+history_list = []
 
 class ClientHandler(socketserver.BaseRequestHandler):
     """
@@ -13,6 +19,10 @@ class ClientHandler(socketserver.BaseRequestHandler):
     only connected clients, and not the server itself. If you want to write
     logic for the server, you must write it outside this class
     """
+
+    username = ""
+    timestamp = ""
+    is_logged_in = False
 
     def handle(self):
         """
@@ -24,10 +34,78 @@ class ClientHandler(socketserver.BaseRequestHandler):
 
         # Loop that listens for messages from the client
         while True:
+            selftimestamp = str(datetime.datetime.now())
             received_string = self.connection.recv(4096)
-            
-            # TODO: Add handling of received payload from client
+            decoded_object = json.loads(received_string)
+            request = decoded_object['request']
+            content = decoded_object['content']
+            if request == 'login':
+                self.login(content)
+            elif request == 'logout':
+                self.logout()
+            elif request == 'names':
+                self.names()
+            elif request == 'help':
+                self.help()
+            elif request == 'msg':
+                self.logout()
+            elif request == 'history':
 
+
+    def login(self, username):
+        global clients
+        self.username = username
+        self.is_logged_in = True
+        clients[username] = self
+        self.send_respone('server', 'info', 'Login successful!', False)
+
+    def logout(self):
+        global clients
+        del clients[self.username]
+        self.send_respone('server', 'info', 'Logout successful', False)
+
+    def names(self):
+        names = []
+        for username in clients:
+            names.append(username)
+        strigToReturn = 'All users in this channel: ', names
+        self.send_respone('server', 'info', strigToReturn, False)
+
+    def help(self):
+        pass
+
+    def msg(self, message):
+        self.send_response(self.username, 'message', message, True)
+
+    def history(self):
+        self.send_response('server', 'history', history_list, False)
+
+    def send_respone(self, sender, response, message, send_to_all):
+
+        if response != 'help' and response != 'login' and not self.is_logged_in:
+            raw_respone = {
+                'timestamp': self.timestamp,
+                'sender': 'server',
+                'response': 'error',
+                'content': 'You need to log in!'
+            }
+        else:
+            raw_respone = {
+                'timestamp': self.timestamp,
+                'sender': sender,
+                'response': response,
+                'content': message
+            }
+        JSON_response = json.dumps(raw_respone)
+
+        if response == 'message':
+            global history_list
+            history_list.append(JSON_response)
+        if send_to_all:
+            for username, client in clients:
+                client.connection.send(JSON_response)
+        else:
+            self.connection.send(JSON_response)
 
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     """
